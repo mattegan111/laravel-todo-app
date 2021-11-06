fc1c61a72efce851073f3358a425f05abd38a1af
mattegan111 <mnegan93@gmail.com>
2021-11-06 22:10:59 +1300

    Add nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,19 @@
     <title>Todos</title>
 </head>
     <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/">Todos App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/todos">Todos</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
         <div class="container">
             @yield('content')
         </div>
