206b12b6f021d35e180859de192c3723b9979ab9
mattegan111 <mnegan93@gmail.com>
2021-11-07 16:45:05 +1300

    Update styling for buttons

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -21,7 +21,7 @@ Todos
                         <a href="/todos/{{ $todo->id }}/delete" class="btn btn-danger btn-sm mx-1" style="float:right"> 
                             Delete
                         </a>
-                        <a href="/todos/{{ $todo->id }}/edit" class="btn btn-primary btn-sm mx-1" style="float:right"> 
+                        <a href="/todos/{{ $todo->id }}/edit" class="btn btn-success btn-sm mx-1" style="float:right"> 
                             Edit
                         </a>
                         <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm" style="float:right"> 
